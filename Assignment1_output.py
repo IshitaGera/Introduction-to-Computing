ff6d524db7f963b5e3601116e6bde62670096388
IshitaGera <96744689+IshitaGera@users.noreply.github.com>
2022-01-14 12:29:04 +0530

    Add files via upload

---
+++ Assignment1_output.py
@@ -0,0 +1,37 @@
+#ques1
+enter first num:10
+enter second num:20
+enter third num:30
+average is: 20.0
+
+
+#ques2
+gross income is:500000
+number of dependents are:2
+taxable income is: 484000.0
+tax to be paid: 96800.0
+
+
+#ques3
+SID of the student:21103094
+name of the student:Ishita
+gender of the student(F/M/U)(U for unknown):F
+course enrolled by the student:CSE
+cgpa obtained by the student:10
+[21103094, 'Ishita', 'F', 'CSE', 10.0]
+
+
+#ques4
+marks obtained by student 1:66
+marks obtained by student 2:98
+marks obtained by student 3:93
+marks obtained by student 4:88
+marks obtained by student 5:90
+[66.0, 88.0, 90.0, 93.0, 98.0]
+
+
+#ques5
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'pink', 'yellow']
+['red', 'green', 'white', 'purple', 'yellow']
+
