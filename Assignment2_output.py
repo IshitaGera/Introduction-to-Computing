4d044f1ceba63ff6e3f376d699c1b6eddc96eef3
IshitaGera <96744689+IshitaGera@users.noreply.github.com>
2022-01-26 14:21:47 +0530

    Add files via upload

---
+++ Assignment2_output.py
@@ -0,0 +1,54 @@
+#ques1
+INPUT STRING =  Python is a case sensitive language
+(Answer of a)
+35
+(Answer of b)
+egaugnal evitisnes esac a si nohtyP
+(Answer of c)
+a case sensitive
+(Answer of d)
+Python is object oriented language
+(Answer of e)
+10
+(Answer of f)
+Pythonisacasesensitivelanguage
+
+
+#ques2
+Hey,Ishita Gera Here!
+My SID is 21103094
+I am from CSE department and my CGPA is 9.9
+
+
+#ques3
+Answer of A
+8
+Answer of B
+58
+Answer of C
+50
+Answer of D
+224
+40
+Answer of E
+14
+0
+
+
+#ques4
+Give a number:20
+Give a number:35
+Give a number:15
+The greatest number is  35
+
+
+#ques5
+Enter the string: my name is ishita
+Yes
+
+
+#ques6
+Give a number:20
+Give a number:35
+Give a number:15
+The triangle is not possible
