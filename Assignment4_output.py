b9d3a5bc32d4cffceb5c99e09fa31ec6caf56b53
IshitaGera <96744689+IshitaGera@users.noreply.github.com>
2022-02-27 15:10:32 +0530

    Add files via upload

---
+++ Assignment4_output.py
@@ -0,0 +1,65 @@
+========= RESTART: C:\Users\Dell\OneDrive\Desktop\Assignment4_input.py =========
+#QUESTION1
+
+Move disk 1 from source A to destination B.
+Move disk 2 from source A to destination C.
+Move disk 1 from source B to destination C.
+Move disk 3 from source A to destination B.
+Move disk 1 from source C to destination A.
+Move disk 2 from source C to destination B.
+Move disk 1 from source A to destination B.
+
+#QUESTION2
+
+Enter the size of pascals triangle 4
+USING FOR LOOP
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+
+
+
+USING WHILE LOOP
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+
+
+
+USING RECURSSIONS
+      1   
+    1   1   
+  1   2   1   
+1   3   3   1   
+
+#QUESTION3
+
+Enter 2 numbers: 9 14
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+b. The quotient is zero
+c. Filtered out numbers that are greater than 4 are : [13, 14, 5, 14, 6, 15]
+d. Set: {5, 6, 13, 14, 15}
+e. Immutable set: frozenset({5, 6, 13, 14, 15})
+f. Hash value of the max value from the above set: 15
+
+#QUESTION4
+
+Object created
+The name of the student is Ishita and SID is 21103094.
+Object destroyed
+
+#QUESTION5
+
+a. The updated salary of Mehak is 70000 
+b. Record of Viren deleted
+
+#QUESTION6
+
+Enter the word: pot
+Enter a new meaningful word using the exact same letters to test your friendship: top
+Does the word makes sense?(y/Y or n/N ) y
+The friends pass the friendship test
